d9afef56f924e3114df480763acbdf15fe1c6a3a
willshepp28 <willsheppard29@gmail.com>
2023-08-09 15:27:13 -0400

    cleanup app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const { User } = require("../models");
 const { setupMiddlewares } = require('./middlewares');
 const { setupRoutes } = require('./routes');
 
@@ -9,19 +8,6 @@ const PORT = process.env.PORT || 5001;
 
 setupMiddlewares(application);
 setupRoutes(application);
-
-// // Signup route here...
-// application.get("/", async(request, response) => {
-//   try {
-//     const users = await User.findAll({
-//       attributes: ['firstName', 'lastName'],
-//     }); 
-//     response.json({ success: true, data: users });
-//   } catch (error) {
-//     console.error("Error fetching users:", error);
-//     response.status(500).json({ success: false, message: "Error fetching users." });
-//   }
-// })
 
 application.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
