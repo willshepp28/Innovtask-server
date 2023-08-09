7068b308fd987bb28237bfd3c053b1ea15674e72
willshepp28 <willsheppard29@gmail.com>
2023-08-09 17:13:47 -0400

    setup authentication routes

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -4,32 +4,23 @@ const jwt = require('jsonwebtoken');
 const { User } = require("../../models");
 
 const router = express.Router();
-const saltRounds = 10;
 const jwtSecret = process.env.JWT_SECRET;
 
 router.post('/signup', async (request, response) => {
     try {
-        // Check if user already exists
         const existingUser = await User.findOne({ where: { email: request.body.email } });
         if (existingUser) {
             return response.status(400).json({ success: false, message: 'Email already in use.' });
         }
 
-        // Hash the password and create user
-        const hashedPassword = await bcrypt.hash(request.body.password, saltRounds);
-        const user = await User.create({
+        const newUser = await User.create({
             email: request.body.email,
             firstName: request.body.firstName,
             lastName: request.body.lastName,
-            password: hashedPassword
+            password: request.body.password
         });
 
-        // Create a JWT
-        const token = jwt.sign({ id: user.id, email: user.email }, jwtSecret, {
-            expiresIn: '14d'  // Token expiration time. Adjust as needed.
-        });
-
-        response.status(201).json({ success: true, token: token });
+        response.status(201).json({ success: true, message: 'User registered successfully.' });
 
     } catch (error) {
         console.error("Error during signup:", error);
@@ -37,4 +28,28 @@ router.post('/signup', async (request, response) => {
     }
 });
 
+router.post('/login', async (request, response) => {
+    try {
+        const user = await User.findOne({ where: { email: request.body.email } });
+        if (!user) {
+            return response.status(401).json({ success: false, message: 'User not found.' });
+        }
+
+        if (!user.validPassword(request.body.password)) {
+            return response.status(401).json({ success: false, message: 'Invalid password.' });
+        }
+
+        const token = jwt.sign({ id: user.id, email: user.email }, jwtSecret, {
+            expiresIn: '14d'  // Token expiration time. Adjust as needed.
+        });
+
+        response.status(200).json({ success: true, token: token });
+
+    } catch (error) {
+        console.error("Error during login:", error);
+        response.status(500).json({ success: false, message: "Internal server error." });
+    }
+});
+
 module.exports = router;
+
