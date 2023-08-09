4d08ce2313864f0f3fd93882894402df73bd9913
willshepp28 <willsheppard29@gmail.com>
2023-08-09 14:20:28 -0400

    placed middlewares in its own file

--- src/app.js
+++ src/app.js
@@ -1,32 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const { Sequelize } = require('sequelize');
-const config = require('../config/config'); // Import your configuration
-const { User } = require("../models")
+const { User } = require("../models");
+const { setupMiddlewares } = require('./middlewares');
 
 const application = express();
 const PORT = process.env.PORT || 5001;
 
-// CORS Configuration
-const whitelist = ['https://innovtask.com'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-application.use(cors(corsOptions));
+// // Initialize env before using i
+setupMiddlewares(application);
 application.use(express.json());
-
-// Get the appropriate configuration based on your environment
-const env = process.env.NODE_ENV || 'development';
 
 // Signup route here...
 application.get("/", async(request, response) => {
---
+++ src/middlewares/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+const env = process.env.NODE_ENV || 'development';
+const whitelist = ['https://innovtask.com'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+function setupMiddlewares(app) {
+  if (env === 'production') {
+    app.use(cors(corsOptions));
+  } else {
+    app.use(cors());
+  }
+
+  app.use(express.json());
+}
+
+module.exports = {
+  setupMiddlewares
+};
