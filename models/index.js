19772ff000824892333838b55e98febae5eb3b3b
willshepp28 <willsheppard29@gmail.com>
2023-08-09 10:26:23 -0400

    added heroku config info

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
+console.log(config)
 fs
   .readdirSync(__dirname)
   .filter(file => {
