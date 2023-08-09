85d2626bd478ef77f8a0dd0edfebe07faf92f683
willshepp28 <willsheppard29@gmail.com>
2023-08-09 09:44:06 -0400

    setup sequelize

--- config/config.js
+++ config/config.js
@@ -16,10 +16,7 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE_PROD,
-    "host": process.env.DB_HOST,
+    "url": process.env.DATABASE_URL,
     "dialect": "postgres",
     "dialectOptions": {
       "ssl": {
--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { Sequelize } = require('sequelize');
+const config = require('../config/config'); // Import your configuration
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const application = express();
+const PORT = process.env.PORT || 5001;
+
+application.use(cors());
+application.use(express.json());
+
+// Get the appropriate configuration based on your environment
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
+
+// Create a Sequelize instance
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.dialect,
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Test the database connection and create it if it doesn't exist
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
+  .catch(async (error) => {
+    if (error.original.code === '3D000') {
+      console.log('Database does not exist. Creating...');
+      await sequelize.query(`CREATE DATABASE "${dbConfig.database}"`);
+      console.log('Database created successfully.');
+    } else {
+      console.error('Unable to connect to the database:', error);
+    }
+  });
+
+// Signup route here...
+application.get("/", (request, response) => {
+  response.json({ message: "Successful"})
+})
+
+application.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  sequelize.sync();
 });
