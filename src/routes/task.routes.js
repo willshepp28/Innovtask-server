d92069c02a571bed80ca5045e602d3c7055b87b7
willshepp28 <willsheppard29@gmail.com>
2023-08-10 15:00:52 -0400

    created task post route

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -15,4 +15,24 @@ router.get("/", authenticateJWT, async (request, response) => {
   response.status(200).json(tasks);
 });
 
+router.post("/", authenticateJWT, async(request, response) => {
+  const { title, description, priority } = request.body;
+  const userId = request.user.id;
+
+  try {
+    const newTask = await Task.create({
+      title,
+      description,
+      priority,
+      userId
+    })
+
+    response.status(401).json(newTask);
+  } catch(error) {
+    console.error("Error creating task:", error);
+    response.status(500).json({ message: "Error creating task" });
+  }
+})
+
+
 module.exports = router;
