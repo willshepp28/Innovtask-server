4865e268a8c641cefcf17617f314e812dcc43921
willshepp28 <willsheppard29@gmail.com>
2023-08-18 16:56:22 -0500

    updated status code in task POST route

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -26,7 +26,7 @@ router.post("/", authenticateJWT, async (request, response) => {
       userId,
     });
 
-    response.status(401).json(newTask);
+    response.status(201).json(newTask);
   } catch (error) {
     console.error("Error creating task:", error);
     response.status(500).json({ message: "Error creating task" });
